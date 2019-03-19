7de8ab8f8581ffac430f3f8d94148a47cbdd7c42
GDemoulin <gregoiredemoulin2@gmail.com>
2019-03-19 15:49:37 +0100

    Add install cchaddet

--- installer.py
+++ installer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+
+def install(alsi):
+    alsi.pip_install_all([
+        'cchardet'
+    ])
+
+
 if __name__ == '__main__':
     from assemblyline.al.install import SiteInstaller
-    SiteInstaller()
+    install(SiteInstaller())
